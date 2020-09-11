fd2de214ea64e86b35c77c32491d96b6758325bc
milangeorge2000 <45516866+milangeorge2000@users.noreply.github.com>
2020-09-11 19:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,20 +18,20 @@ def home():
 
 
 
-# @app.route('/webhook', methods=['POST'])
-# def webhook():
+@app.route('/webhook', methods=['POST'])
+def webhook():
 
-#     req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent=True, force=True)
 
-#     #print("Request:")
-#     #print(json.dumps(req, indent=4))
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
 
-#     res = processRequest(req)
-#     res = json.dumps(res, indent=4)
-#     #print(res)
-#     r = make_response(res)
-#     r.headers['Content-Type'] = 'application/json'
-#     return r
+    res = processRequest(req)
+    res = json.dumps(res, indent=4)
+    #print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 
 # def processRequest(req):
